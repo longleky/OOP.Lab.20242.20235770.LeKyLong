eb349d3d6adf3f09d7449f5a6b2c612afaba6ee6
longleky <lekylong2k5@gmail.com>
2025-03-01 22:52:53 +0700

    Create baitap225.java

---
+++ Lab 01/baitap225.java
@@ -0,0 +1,28 @@
+import javax.swing.JOptionPane;
+public class baitap225{
+    public static void main(String[] args){
+        String strNum1,strNum2;
+        String strNotification = "Sum: ";
+
+        strNum1 = JOptionPane.showInputDialog(null,"Please input the first number : ","Input the first number",JOptionPane.INFORMATION_MESSAGE);
+        double num1 = Double.parseDouble(strNum1);
+        strNum2 = JOptionPane.showInputDialog(null,"Please input the second number : ","Input the second number",JOptionPane.INFORMATION_MESSAGE);
+        double num2 = Double.parseDouble(strNum2);
+        double sum = num1 + num2;
+        double difference = num1 - num2;
+        double product = num1 * num2;
+        String divisionResult;
+        if (num2 == 0) {
+            divisionResult = "Cannot divide by zero!";
+        } else {
+            double quotient = num1 / num2;
+            divisionResult = "Quotient: " + quotient;
+        }
+        strNotification += sum + "\n"
+                         + "Difference: " + difference + "\n"
+                         + "Product: " + product + "\n"
+                         + divisionResult; 
+        JOptionPane.showMessageDialog(null,strNotification,"show two number", JOptionPane.INFORMATION_MESSAGE);
+        System.exit(0);
+    }
+}
